088be2eef510b577d75ad36701854c0b931ab01a
Isai de Loza <isai.deloza@contracted.pmi.com>
2020-08-27 14:41:55 -0500

    adding aws values

--- javascript/example_code/s3/s3_PhotoViewer.js
+++ javascript/example_code/s3/s3_PhotoViewer.js
@@ -36,27 +36,27 @@
 // **DO THIS**:
 //   Replace BUCKET_NAME with the bucket name.
 //
-var albumBucketName = 'BUCKET_NAME';
+var albumBucketName = 'casi-pa-s3-js-demo';
 
 // **DO THIS**:
 //   Replace this block of code with the sample code located at:
 //   Cognito -- Manage Identity Pools -- [identity_pool_name] -- Sample Code -- JavaScript
 //
 // Initialize the Amazon Cognito credentials provider
-AWS.config.region = 'REGION'; // Region
+AWS.config.region = 'eu-west-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'IDENTITY_POOL_ID',
+    IdentityPoolId: 'eu-west-1:b1d303fe-b29f-40b1-a711-0d9dda0e5022',
 });
 
 // Create a new service object
 var s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {Bucket: albumBucketName}
+    apiVersion: '2006-03-01',
+    params: { Bucket: albumBucketName }
 });
 
 // A utility function to create HTML.
 function getHtml(template) {
-  return template.join('\n');
+    return template.join('\n');
 }
 // snippet-end:[s3.JavaScript.s3_PhotoViewer.config]
 
@@ -68,96 +68,96 @@ function getHtml(template) {
 // snippet-start:[s3.JavaScript.s3_PhotoViewer.listAlbums]
 // List the photo albums that exist in the bucket.
 function listAlbums() {
-  s3.listObjects({Delimiter: '/'}, function(err, data) {
-    if (err) {
-      return alert('There was an error listing your albums: ' + err.message);
-    } else {
-      var albums = data.CommonPrefixes.map(function(commonPrefix) {
-        var prefix = commonPrefix.Prefix;
-        var albumName = decodeURIComponent(prefix.replace('/', ''));
-        return getHtml([
-          '<li>',
-            '<button style="margin:5px;" onclick="viewAlbum(\'' + albumName + '\')">',
-              albumName,
-            '</button>',
-          '</li>'
-        ]);
-      });
-      var message = albums.length ?
-        getHtml([
-          '<p>Click on an album name to view it.</p>',
-        ]) :
-        '<p>You do not have any albums. Please Create album.';
-      var htmlTemplate = [
-        '<h2>Albums</h2>',
-        message,
-        '<ul>',
-          getHtml(albums),
-        '</ul>',
-      ]
-      document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
-    }
-  });
+    s3.listObjects({ Delimiter: '/' }, function(err, data) {
+        if (err) {
+            return alert('There was an error listing your albums: ' + err.message);
+        } else {
+            var albums = data.CommonPrefixes.map(function(commonPrefix) {
+                var prefix = commonPrefix.Prefix;
+                var albumName = decodeURIComponent(prefix.replace('/', ''));
+                return getHtml([
+                    '<li>',
+                    '<button style="margin:5px;" onclick="viewAlbum(\'' + albumName + '\')">',
+                    albumName,
+                    '</button>',
+                    '</li>'
+                ]);
+            });
+            var message = albums.length ?
+                getHtml([
+                    '<p>Click on an album name to view it.</p>',
+                ]) :
+                '<p>You do not have any albums. Please Create album.';
+            var htmlTemplate = [
+                '<h2>Albums</h2>',
+                message,
+                '<ul>',
+                getHtml(albums),
+                '</ul>',
+            ]
+            document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
+        }
+    });
 }
 // snippet-end:[s3.JavaScript.s3_PhotoViewer.listAlbums]
 
 // snippet-start:[s3.JavaScript.s3_PhotoViewer.viewAlbum]
 // Show the photos that exist in an album.
 function viewAlbum(albumName) {
-  var albumPhotosKey = encodeURIComponent(albumName) + '/';
-  s3.listObjects({Prefix: albumPhotosKey}, function(err, data) {
-    if (err) {
-      return alert('There was an error viewing your album: ' + err.message);
-    }
-    // 'this' references the AWS.Response instance that represents the response
-    var href = this.request.httpRequest.endpoint.href;
-    var bucketUrl = href + albumBucketName + '/';
+    var albumPhotosKey = encodeURIComponent(albumName) + '/';
+    s3.listObjects({ Prefix: albumPhotosKey }, function(err, data) {
+        if (err) {
+            return alert('There was an error viewing your album: ' + err.message);
+        }
+        // 'this' references the AWS.Response instance that represents the response
+        var href = this.request.httpRequest.endpoint.href;
+        var bucketUrl = href + albumBucketName + '/';
 
-    var photos = data.Contents.map(function(photo) {
-      var photoKey = photo.Key;
-      var photoUrl = bucketUrl + encodeURIComponent(photoKey);
-      return getHtml([
-        '<span>',
-          '<div>',
-            '<br/>',
-            '<img style="width:128px;height:128px;" src="' + photoUrl + '"/>',
-          '</div>',
-          '<div>',
-            '<span>',
-              photoKey.replace(albumPhotosKey, ''),
-            '</span>',
-          '</div>',
-        '</span>',
-      ]);
+        var photos = data.Contents.map(function(photo) {
+            var photoKey = photo.Key;
+            var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+            return getHtml([
+                '<span>',
+                '<div>',
+                '<br/>',
+                '<img style="width:128px;height:128px;" src="' + photoUrl + '"/>',
+                '</div>',
+                '<div>',
+                '<span>',
+                photoKey.replace(albumPhotosKey, ''),
+                '</span>',
+                '</div>',
+                '</span>',
+            ]);
+        });
+        var message = photos.length ?
+            '<p>The following photos are present.</p>' :
+            '<p>There are no photos in this album.</p>';
+        var htmlTemplate = [
+            '<div>',
+            '<button onclick="listAlbums()">',
+            'Back To Albums',
+            '</button>',
+            '</div>',
+            '<h2>',
+            'Album: ' + albumName,
+            '</h2>',
+            message,
+            '<div>',
+            getHtml(photos),
+            '</div>',
+            '<h2>',
+            'End of Album: ' + albumName,
+            '</h2>',
+            '<div>',
+            '<button onclick="listAlbums()">',
+            'Back To Albums',
+            '</button>',
+            '</div>',
+        ]
+        document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
+        document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
     });
-    var message = photos.length ?
-      '<p>The following photos are present.</p>' :
-      '<p>There are no photos in this album.</p>';
-    var htmlTemplate = [
-      '<div>',
-        '<button onclick="listAlbums()">',
-          'Back To Albums',
-        '</button>',
-      '</div>',
-      '<h2>',
-        'Album: ' + albumName,
-      '</h2>',
-      message,
-      '<div>',
-        getHtml(photos),
-      '</div>',
-      '<h2>',
-        'End of Album: ' + albumName,
-      '</h2>',
-      '<div>',
-        '<button onclick="listAlbums()">',
-          'Back To Albums',
-        '</button>',
-      '</div>',
-    ]
-    document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
-    document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
-  });
 }
 // snippet-end:[s3.JavaScript.s3_PhotoViewer.viewAlbum]
 // snippet-end:[s3.JavaScript.s3_PhotoViewer.complete]
